19b134b237ea76ed299db06a88c6037d98d662b4
Nyoik3 <nyoike.derrick.ndungu@gmail.com>
2022-06-02 09:23:10 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - prints statement
+ * main - main statement
  * return: 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
