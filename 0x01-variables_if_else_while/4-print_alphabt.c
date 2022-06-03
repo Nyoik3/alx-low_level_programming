f596ed2abdcb3d841162e09f8b3a8d74fce21746
Nyoik3 <nyoike.derrick.ndungu@gmail.com>
2022-06-03 11:43:11 -0700

    print alphabet except e q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,13 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a' ; ch = 'z' ; ch++)
+	while (ch <= 'z')
 	{
 	if (ch != 'e' && ch != 'q')
 	{
 	putchar(ch);
 	}
+	ch++;
 	}
 	putchar('\n');
 	return (0);
