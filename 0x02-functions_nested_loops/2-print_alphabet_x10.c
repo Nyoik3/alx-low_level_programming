185746da46520addfe1f5f6ccd87274deece91a4
Nyoik3 <nyoike.derrick.ndungu@gmail.com>
2022-06-07 09:51:25 -0700

    ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet ten times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+	ch = 'a';
+	while (ch <= 'z')
+	{
+	_putchar(ch);
+	ch++;
+	}
+	_putchar('\n');
+	i++;
+	}
+}
+
