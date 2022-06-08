658b3b9261bf0de4ed3427d41cc0a7788a85f373
Nyoik3 <nyoike.derrick.ndungu@gmail.com>
2022-06-08 04:31:28 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,37 +10,27 @@ void times_table(void)
 	int a;
 	int b;
 	int c;
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 9; b++);
+		for (b = 0; b < 10; b++);
 		{
 			c = a * b;
-			if ((c / 10 ) == 0)
-			{
 			if (b == 0)
 			{
-			_putchar('0');
+				_putchar(c + '0');
 			}
-			if (b != 0)
+			if (c < 10 && b != 0)
 			{
-			_putchar(' ');
-			_putchar((c % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(c + '0');
 			}
-			if (b < 9)
+			else if (c >= 10)
 			{
-			_putchar(',');
-			_putchar(' ');
-			}
-			}
-			else
-			{
-			_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
-			if (b < 9)
-			{
-			_putchar(',');
-			_putchar(' ');
-			}
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
 		}
 	_putchar ('\n');
