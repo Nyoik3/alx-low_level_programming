70dee1d28b8cbada61fe3af211e9f213b8c88c58
Nyoik3 <nyoike.derrick.ndungu@gmail.com>
2022-06-15 11:13:27 -0700

    string comparison

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,10 +13,10 @@ int _strcmp(char *s1, char *s2)
 
 	while (ab == 0)
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-					break;
-		ab = *(s1 + i) - *(s2 + i);
-		i++;
+	if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+		break;
+	ab = *(s1 + i) - *(s2 + i);
+	i++;
 	}
 	return (ab);
 }
