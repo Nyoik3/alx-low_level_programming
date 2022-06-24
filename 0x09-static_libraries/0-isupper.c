75191203c134e5a491d1419227105b87b8d11b03
Nyoik3 <nyoike.derrick.ndungu@gmail.com>
2022-06-24 14:37:51 -0700

    static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _isupper - evaluates if letter is uppercase
+ *
+ *   * @c: print in
+ *
+ *    * Return: 0
+ *
+ *     */
+
+int _isupper(int c)
+
+{
+
+		if (c >= 'A' && c <= 'Z')
+
+				{
+
+							return (1);
+
+								}
+
+			else
+
+					{
+
+								return (0);
+
+									}
+
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _islower - lowercase alphabet checker
+ *
+ *   * @c: the character to check
+ *
+ *    * Return: 1 if c is lower cas , o if otherwise
+ *
+ *     */
+
+int _islower(int c)
+
+{
+
+		return (c >= 'a' && c <= 'z');
+
+}
